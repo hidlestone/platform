619e93c26cd5a2bcdb839984ab2f2e47d31def6a
zhuangpf <payn_809566095>
2022-03-22 10:28:26 +0800

    feat: starter

---
+++ fall-platform-starter-api-dependency/src/main/java/com/fallframework/platform/starter/api/model/NYEnum.java
@@ -0,0 +1,19 @@
+package com.fallframework.platform.starter.api.model;
+
+/**
+ * 全系统通用是否枚举
+ *
+ * @author zhuangpf
+ */
+public enum NYEnum {
+
+	/**
+	 * 0-否
+	 */
+	N,
+	/**
+	 * 1-是
+	 */
+	Y;
+
+}
