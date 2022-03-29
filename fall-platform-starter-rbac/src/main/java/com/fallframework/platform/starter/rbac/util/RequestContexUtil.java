d4df32cf3ef6eb29ff3edb2980b9d634f1c17cd0
zhuangpf <payn_809566095>
2022-03-29 15:20:23 +0800

    feat: starter

--- fall-platform-starter-rbac/src/main/java/com/fallframework/platform/starter/rbac/util/RequestContexUtil.java
+++ fall-platform-starter-rbac/src/main/java/com/fallframework/platform/starter/rbac/util/RequestContexUtil.java
@@ -1,5 +1,6 @@
 package com.fallframework.platform.starter.rbac.util;
 
+import com.fallframework.platform.starter.rbac.constant.RbacStarterConstant;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
@@ -18,8 +19,8 @@ public class RequestContexUtil {
 	 */
 	public static void setTokenHeader(String accesstoken, String refreshtoken) {
 		HttpServletResponse response = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getResponse();
-		response.setHeader("accesstoken", accesstoken);
-		response.setHeader("refreshtoken", refreshtoken);
+		response.setHeader(RbacStarterConstant.ACCESSTOKEN, accesstoken);
+		response.setHeader(RbacStarterConstant.REFRESHTOKEN, refreshtoken);
 	}
 
 }
