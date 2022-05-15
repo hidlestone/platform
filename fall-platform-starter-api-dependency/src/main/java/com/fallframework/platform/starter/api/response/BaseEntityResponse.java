dcd4e567f6aa3b982313c979043758de82d9bb44
zhuangpf <809566095@qq.com>
2022-05-15 16:03:11 +0800

    feat: 优化

--- fall-platform-starter-api-dependency/src/main/java/com/fallframework/platform/starter/api/response/BaseEntityResponse.java
+++ fall-platform-starter-api-dependency/src/main/java/com/fallframework/platform/starter/api/response/BaseEntityResponse.java
@@ -1,9 +1,11 @@
 package com.fallframework.platform.starter.api.response;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.io.Serializable;
+import java.util.Date;
 
 /**
  * 基础响应参数类
@@ -15,5 +17,17 @@ import java.io.Serializable;
 public class BaseEntityResponse implements Serializable {
 
 	private static final long serialVersionUID = 4349971885970231113L;
+
+	@ApiModelProperty("创建用户ID")
+	private Long createUserId;
+
+	@ApiModelProperty("创建用户ID")
+	private Long modifyUserId;
+
+	@ApiModelProperty("创建时间")
+	private Date gmtCreate;
+
+	@ApiModelProperty("更改时间")
+	private Date gmtModified;
 
 }
