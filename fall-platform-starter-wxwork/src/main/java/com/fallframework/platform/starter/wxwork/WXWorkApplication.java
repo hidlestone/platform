f5af3029a237f17699642779c54e1f0ef769f3b3
zhuangpf <809566095@qq.com>
2022-06-10 17:42:56 +0800

    feat: 添加模块

---
+++ fall-platform-starter-wxwork/src/main/java/com/fallframework/platform/starter/wxwork/WXWorkApplication.java
@@ -0,0 +1,16 @@
+package com.fallframework.platform.starter.wxwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhuangpf
+ */
+@SpringBootApplication
+public class WXWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WechatcpApplication.class, args);
+	}
+
+}
