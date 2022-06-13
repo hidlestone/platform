848e5acbf75026e609a290778470c5704625fb45
zhuangpf <809566095@qq.com>
2022-06-13 11:37:48 +0800

    feat: 整合企微

--- fall-platform-starter-wxwork/src/main/java/com/fallframework/platform/starter/wxwork/constant/WxworkStarterConstant.java
+++ fall-platform-starter-wxwork/src/main/java/com/fallframework/platform/starter/wxwork/constant/WxworkStarterConstant.java
@@ -8,6 +8,24 @@ public class WxworkStarterConstant {
 	/**
 	 * 缓存key：accesstoken
 	 */
-	public static final String CACHE_KEY_ACCESS_TOKEN = "wxwork:accesstoken:";
+	public static final String CACHE_KEY_ACCESS_TOKEN = "wxwork:accesstoken";
+
+	// ----------------------------------------- token ----------------------------------------- //
+	/**
+	 * 获取token
+	 */
+	public static final String URL_GETTOKEN = "https://qyapi.weixin.qq.com/cgi-bin/gettoken";
+
+	// ----------------------------------------- 通讯录 ----------------------------------------- //
+	/**
+	 * 创建成员
+	 */
+	public static final String URL_USER_CREATE = "https://qyapi.weixin.qq.com/cgi-bin/user/create?access_token=ACCESS_TOKEN";
+
+	/**
+	 * 读取成员
+	 */
+	public static final String URL_USER_GET = "https://qyapi.weixin.qq.com/cgi-bin/user/get?access_token=ACCESS_TOKEN&userid=USERID";
+
 
 }
--- fall-platform-starter-wxwork/src/main/java/com/fallframework/platform/starter/wxwork/service/contact/UserService.java
+++ fall-platform-starter-wxwork/src/main/java/com/fallframework/platform/starter/wxwork/service/contact/UserService.java
@@ -2,15 +2,20 @@ package com.fallframework.platform.starter.wxwork.service.contact;
 
 import com.alibaba.fastjson.JSON;
 import com.fallframework.platform.starter.api.response.ResponseResult;
+import com.fallframework.platform.starter.cache.redis.util.RedisUtil;
+import com.fallframework.platform.starter.wxwork.config.WxCpProperties;
+import com.fallframework.platform.starter.wxwork.constant.WxworkStarterConstant;
+import com.fallframework.platform.starter.wxwork.dto.GetAccessTokenDto;
+import com.fallframework.platform.starter.wxwork.model.AccessTokenTypeEnum;
 import com.fallframework.platform.starter.wxwork.model.TokenResponse;
 import com.fallframework.platform.starter.wxwork.service.token.TokenService;
-import com.google.gson.Gson;
 import okhttp3.FormBody;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -23,9 +28,12 @@ import java.util.Map;
 @Service
 public class UserService {
 
-	private static final String URL_CREATE = "https://qyapi.weixin.qq.com/cgi-bin/user/create";
+	@Autowired
+	private WxCpProperties wxCpProperties;
 	@Autowired
 	private TokenService tokenService;
+	@Autowired
+	private RedisUtil redisUtil;
 
 	/**
 	 * 创建用户
@@ -34,23 +42,54 @@ public class UserService {
 	 * @return 是否创建成功
 	 */
 	public ResponseResult create(Map<String, Object> userMap) throws IOException {
-		// 获取accesstoken
-		String ACCESS_TOKEN = null;
-		
-
-		String url = URL_CREATE + "?" + "access_token=" + ACCESS_TOKEN;
+		// 从缓存中获取通讯录accesstoken
+		String access_token = getAccessToken();
+		String url = WxworkStarterConstant.URL_USER_CREATE.replace("ACCESS_TOKEN", access_token);
 		// 请求
 		RequestBody requestBody = FormBody.create(
 				MediaType.parse("application/json"), JSON.toJSONString(userMap));
 		Response response = new OkHttpClient().newCall(new Request.Builder().url(url).post(requestBody).build()).execute();
-		TokenResponse tokenResponse = new Gson().fromJson(response.body().string(), TokenResponse.class);
-		if (response.code() != 200) {
-			return ResponseResult.fail(JSON.toJSONString(tokenResponse));
+		TokenResponse tokenResponse = JSON.parseObject(response.body().string(), TokenResponse.class);
+		if (200 != response.code()) {
+			return ResponseResult.fail(JSON.toJSONString(response));
 		}
 		if (0 != tokenResponse.getErrcode()) {
-			return ResponseResult.fail(JSON.toJSONString(tokenResponse));
+			return ResponseResult.fail(String.valueOf(tokenResponse.getErrcode()), tokenResponse.getErrmsg());
 		}
 		return ResponseResult.success();
+	}
+
+	/**
+	 * 读取用户
+	 *
+	 * @param userId 用户ID
+	 * @return 用户信息
+	 */
+	public ResponseResult get(String userId) throws IOException {
+		// 从缓存中获取通讯录accesstoken
+		String access_token = getAccessToken();
+		String url = WxworkStarterConstant.URL_USER_GET.replace("ACCESS_TOKEN", access_token).replace("USERID", userId);
+		Response response = new OkHttpClient().newCall(new Request.Builder().url(url).get().build()).execute();
+		if (200 != response.code()) {
+			return ResponseResult.fail(JSON.toJSONString(response));
+		}
+		return ResponseResult.success(response.body());
+	}
+
+	/**
+	 * 获取token
+	 */
+	public String getAccessToken() throws IOException {
+		// 从缓存中获取通讯录accesstoken
+		String access_token = (String) redisUtil.hget(WxworkStarterConstant.CACHE_KEY_ACCESS_TOKEN, AccessTokenTypeEnum.CONTACT.name());
+		if (StringUtils.isEmpty(access_token)) {
+			GetAccessTokenDto dto = new GetAccessTokenDto();
+			dto.setCorpId(wxCpProperties.getCorpId());
+			dto.setSecret(wxCpProperties.getContactConfig().getSecret());
+			dto.setAccessTokenType(AccessTokenTypeEnum.CONTACT);
+			access_token = tokenService.gettoken(dto).getData().getAccess_token();
+		}
+		return access_token;
 	}
 
 }
